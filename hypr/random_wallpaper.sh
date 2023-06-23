6986d6141735c90d339da4aed2db27ca8a6e08a8
dousbao <dousbao.cjx@gmail.com>
2023-06-23 12:32:06 +0800

    fix: random wallpaper doesn't seem that prefect. Select the best one.

--- hypr/random_wallpaper.sh
+++ hypr/random_wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
-local wallpaper=$(find $HOME/.local/share/hyprpaper -type f | shuf -n 1)
+# local wallpaper=$(find $HOME/.local/share/hyprpaper -type f | shuf -n 1)
+local wallpaper="$HOME/.local/share/hyprpaper/tablet-2560x1700.jpg"
 echo -ne "preload = $wallpaper\nwallpaper = ,$wallpaper" > $HOME/.config/hypr/hyprpaper.conf
 hyprpaper
